5cede7f012446ee2f5024a2685a1c08e0b84897e
Степанов Михаил <ml.stepanov@jet.su>
2020-03-15 14:07:41 +0300

    Implemented assign, set_value() and batch_set_value(). `update` coming soon.

--- backend/operation.py
+++ backend/operation.py
@@ -19,7 +19,81 @@ class Graph:
 
 
 def get_default_graph():
-    return _graph_registry[Graph]
+    try:
+        return _graph_registry[Graph]
+    except KeyError:
+        Graph().as_default()
+        return _graph_registry[Graph]
+
+
+class Session:
+    """
+    >>> Graph().as_default()
+    >>> A = Variable([[1, 0], [0, -1]])
+    >>> b = Variable([1, 1])
+    >>> x = placeholder()
+    >>> y = matmul(A, x)
+    >>> z = add(y, b)
+    >>> session = Session()
+    >>> output = session.run(z, {x: [1, 2]})
+    >>> output
+    tensor([ 2., -1.])
+    >>> type(output)
+    <class 'torch.Tensor'>
+    >>> output = session.run([z], {x: [1, 2]})
+    >>> output
+    [tensor([ 2., -1.])]
+    """
+
+    def run(self, operation, feed_dict=None, **kwargs):
+        """
+        >>> var = Variable([1.], dtype=torch.float32)
+        >>> ph = placeholder()
+        >>> _ = var.assign(ph)
+        >>> Session().run(var, feed_dict={ph: [0.]})
+        tensor([0.])
+        """
+        feed_dict = feed_dict or {}
+
+        return_as = None
+        if isinstance(operation, list) or isinstance(operation, tuple):
+            return_as = True
+
+        operation = to_list(operation)
+
+        nodes_postorder = traverse_postorder(operation)
+
+        # Note #3: assign operation is specific. It's unary operation. And it does
+        # the following: placeholder -> (assign) -> Variable.
+        # So, I use a trick to ignore assign operations which have placeholders, but
+        # these placeholder just have no relative values in a feed_dict.
+        #
+        # If there's no value for a given placeholder in a dict, you should skip next
+        # assign operation and use current value of the Variable.
+        skip_next_assign = False
+        for index, node in enumerate(nodes_postorder):
+            if isinstance(node, placeholder):
+                try:
+                    node.output = feed_dict[node]
+                except KeyError as e:
+                    if index < len(nodes_postorder) - 1 and isinstance(nodes_postorder[index + 1], assign):
+                        skip_next_assign = True
+                    else:
+                        raise e
+            elif isinstance(node, Variable):
+                node.output = node.value
+            else:
+                if (isinstance(node, assign) and skip_next_assign):
+                    skip_next_assign = False
+                    continue
+                node.inputs = [input_node.output for input_node in node.input_nodes]
+                node.output = node.compute(*node.inputs)
+        result = [op.output for op in operation]
+
+        if return_as is not None:
+            return result
+        else:
+            return unpack_singleton(result)
 
 
 class Operation:
@@ -38,6 +112,95 @@ class Operation:
 
     def __call__(self, *args, **kwargs):
         return self.compute(*args, **kwargs)
+
+
+class assign(Operation):
+    def __init__(self, assignee, source):
+        super().__init__([source])
+        self.assignee = assignee
+        # assignee.input_nodes.append(self)
+
+    def compute(self, value):
+        self.assignee.value = value
+        return self.assignee.value
+
+    def __call__(self, *args, **kwargs):
+        return self.compute(*args, **kwargs)
+
+
+class placeholder:
+    """Represents a placeholder node that has to be provided with a value
+       when computing the output of a computational graph
+    """
+
+    def __init__(self, dtype=None, shape=None):
+        """Construct placeholder
+        """
+        if dtype is None:
+            self.dtype = torch.float32
+        else:
+            self.dtype = dtype
+        self.consumers = []
+        self.shape = shape
+        self._output = None
+
+        # Append this placeholder to the list of placeholders in the currently active default graph
+        get_default_graph().placeholders.append(self)
+
+    @property
+    def output(self):
+        return self._output
+
+    @output.setter
+    def output(self, value):
+        self._output = torch.tensor(value, dtype=self.dtype)
+
+
+class Variable:
+    """Represents a variable (i.e. an intrinsic, changeable parameter of a computational graph).
+    """
+
+    def __init__(self, initial_value=None, dtype=None):
+        """Construct Variable
+
+        Args:
+          initial_value: The initial value of this variable
+        """
+        if dtype is None:
+            dtype = torch.float32
+        self.dtype = dtype
+        if initial_value is not None:
+            self.value = torch.tensor(initial_value, dtype=self.dtype)
+        else:
+            # TODO: what should we do now? Probably I should create initializers
+            pass
+        self.consumers = []
+        # Note #2: there are no such operations by default as _assign_placeholder
+        # But they could be set later in set_value() or another vai another function
+        # Append this variable to the list of variables in the currently active default graph
+        get_default_graph().variables.append(self)
+
+    def assign(self, assign_placeholder):
+        """
+        :param assign_placeholder:
+        :return:
+        >>> graph = Graph().as_default()
+        >>> var = Variable([1.], dtype=torch.float32)
+        >>> ph = placeholder()
+        >>> _ = var.assign(ph)
+        >>> Session().run(var, feed_dict={ph: [0.]})
+        tensor([0.])
+        """
+        # It is should be an operation, so we couldn't just do
+        # self.value = value
+        # But I don't know how to reassign value of tensor inplace in pytorch
+        # self.value = torch.tensor(value, dtype=self.dtype)
+        # source.consumers.append(self)
+        # I'm not sure if it's necessary to set private properties here. Because the will
+        # be set up in the set_value()
+        self._assign_op = assign(self, assign_placeholder)
+        self._assign_placeholder = assign_placeholder
+        return self._assign_op
 
 
 class add(Operation):
@@ -125,51 +288,6 @@ class reduce_sum(Operation):
         return torch.sum(A_value, self.axis)
 
 
-class placeholder:
-    """Represents a placeholder node that has to be provided with a value
-       when computing the output of a computational graph
-    """
-
-    def __init__(self, dtype=None):
-        """Construct placeholder
-        """
-        if dtype is None:
-            self.dtype = torch.float32
-        self.consumers = []
-        self._output = None
-
-        # Append this placeholder to the list of placeholders in the currently active default graph
-        get_default_graph().placeholders.append(self)
-
-    @property
-    def output(self):
-        return self._output
-
-    @output.setter
-    def output(self, value):
-        self._output = torch.tensor(value, dtype=self.dtype)
-
-
-class Variable:
-    """Represents a variable (i.e. an intrinsic, changeable parameter of a computational graph).
-    """
-
-    def __init__(self, initial_value=None, dtype=None):
-        """Construct Variable
-
-        Args:
-          initial_value: The initial value of this variable
-        """
-        if dtype is None:
-            dtype = torch.float32
-        self.dtype = dtype
-        self.value = torch.tensor(initial_value, dtype=self.dtype)
-        self.consumers = []
-
-        # Append this variable to the list of variables in the currently active default graph
-        get_default_graph().variables.append(self)
-
-
 def traverse_postorder(operation):
     """
     >>> Graph().as_default()
@@ -182,66 +300,26 @@ def traverse_postorder(operation):
     >>> session.run(o, {A: [[1, 0], [0, -1]], x: [1, 2]})
     tensor([ 2., -4.])
     """
+    # Todo: use ordered set
     nodes_postorder = []
     nodes_set = set()
 
     def recurse(node):
+        # How to connect assign operations (source of variables) to the main graph?
         if node not in nodes_set:
+            # if isinstance(node, assign):
+
             if isinstance(node, Operation):
                 for input_node in node.input_nodes:
                     recurse(input_node)
+            if isinstance(node, Variable) and hasattr(node, '_assign_op'):
+                recurse(node._assign_op)
             nodes_set.add(node)
             nodes_postorder.append(node)
 
     for op in operation:
         recurse(op)
     return nodes_postorder
-
-
-class Session:
-    """
-    >>> Graph().as_default()
-    >>> A = Variable([[1, 0], [0, -1]])
-    >>> b = Variable([1, 1])
-    >>> x = placeholder()
-    >>> y = matmul(A, x)
-    >>> z = add(y, b)
-    >>> session = Session()
-    >>> output = session.run(z, {x: [1, 2]})
-    >>> output
-    tensor([ 2., -1.])
-    >>> type(output)
-    <class 'torch.Tensor'>
-    >>> output = session.run([z], {x: [1, 2]})
-    >>> output
-    [tensor([ 2., -1.])]
-    """
-
-    def run(self, operation, feed_dict=None, **kwargs):
-        feed_dict = feed_dict or {}
-
-        return_as = None
-        if isinstance(operation, list) or isinstance(operation, tuple):
-            return_as = True
-
-        operation = to_list(operation)
-
-        nodes_postorder = traverse_postorder(operation)
-
-        for node in nodes_postorder:
-            if isinstance(node, placeholder):
-                node.output = feed_dict[node]
-            elif isinstance(node, Variable):
-                node.output = node.value
-            else:
-                node.inputs = [input_node.output for input_node in node.input_nodes]
-                node.output = node.compute(*node.inputs)
-        result = [op.output for op in operation]
-
-        if return_as is not None:
-            return result
-        else:
-            return unpack_singleton(result)
 
 
 class negative(Operation):
@@ -431,3 +509,101 @@ def function(inputs, outputs, updates=None, **kwargs):
     #                    'backend') % key
     #             raise ValueError(msg)
     return Function(inputs, outputs, updates=updates, **kwargs)
+
+
+def _convert_string_dtype(dtype):
+    """Get the type from a string.
+
+    # Arguments
+        dtype: A string representation of a type.
+
+    # Returns
+        The type requested.
+
+    # Raises
+        ValueError: if `dtype` is not supported.
+    """
+    if dtype == 'float16':
+        return torch.float16
+    if dtype == 'float32':
+        return torch.float32
+    elif dtype == 'float64':
+        return torch.float64
+    elif dtype == 'int16':
+        return torch.int16
+    elif dtype == 'int32':
+        return torch.int32
+    elif dtype == 'int64':
+        return torch.int64
+    elif dtype == 'uint8':
+        return torch.int8
+    elif dtype == 'uint16':
+        return torch.uint16  # is it not allowed? # TODO
+    else:
+        raise ValueError('Unsupported dtype:', dtype)
+
+
+def set_value(x, value):
+    """Sets the value of a variable, from a Numpy array.
+
+    # Arguments
+        x: Tensor to set to a new value.
+        value: Value to set the tensor to, as a Numpy array
+            (of the same shape).
+    >>> var = Variable([0.])
+    >>> set_value(var, [1.])
+    >>> Session().run(var)
+    tensor([1.])
+    """
+    value = np.asarray(value)
+    # TODO: make pytorch-specific dtype processing
+    tf_dtype = _convert_string_dtype(str(x.dtype).split('.')[1])
+    # Note #1
+    # tf backend uses _assign_placeholder of Variable to set new values.
+    # Guess I don't need this as for now torchy Varibales are much simpler and don't depend
+    # on tf's (over?)-complicated conceptions
+    # ...
+    # Heck NO. Thinking a little I've got that it was probably a simplest solution.
+    # BUT as a bonus I can use my own Variable class .assign() operation, so I will do it.
+    if hasattr(x, '_assign_placeholder'):
+        assign_placeholder = x._assign_placeholder
+        assign_op = x._assign_op
+    else:
+        assign_placeholder = placeholder(dtype=tf_dtype, shape=value.shape)
+        assign_op = x.assign(assign_placeholder)
+        x._assign_placeholder = assign_placeholder
+        x._assign_op = assign_op
+
+    get_session().run(assign_op, feed_dict={assign_placeholder: value})
+
+
+def batch_set_value(tuples):
+    """Sets the values of many tensor variables at once.
+
+    # Arguments
+        tuples: a list of tuples `(tensor, value)`.
+            `value` should be a Numpy array.
+    >>> batch = [(Variable([0.]), [1.]), (Variable([1.]), [0.])]
+    >>> batch_set_value(batch)
+    >>> Session().run([v for v, _ in batch])
+    [tensor([1.]), tensor([0.])]
+    """
+    # Note #4: batch_set_value use same operations as set_value
+    if tuples:
+        assign_ops = []
+        feed_dict = {}
+        for x, value in tuples:
+            value = np.asarray(value)
+            tf_dtype = _convert_string_dtype(str(x.dtype).split('.')[1])
+            if hasattr(x, '_assign_placeholder'):
+                assign_placeholder = x._assign_placeholder
+                assign_op = x._assign_op
+            else:
+                assign_placeholder = placeholder(dtype=tf_dtype,
+                                                 shape=value.shape)
+                assign_op = x.assign(assign_placeholder)
+                x._assign_placeholder = assign_placeholder
+                x._assign_op = assign_op
+            assign_ops.append(assign_op)
+            feed_dict[assign_placeholder] = value
+        get_session().run(assign_ops, feed_dict=feed_dict)
